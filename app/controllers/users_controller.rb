ebc996c01d466d0622058abe1dd9de3d8105f88c
ayaka.yoshida <eveaeve524@gmail.com>
2019-05-22 08:10:47 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+    # User.order(id: :desc) でidの降順にユーザの一覧を取得
+    # .page(params[:page).per(表示ページ数) ページネーション反映 =>(view側)<%= paginate @messages %>
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
