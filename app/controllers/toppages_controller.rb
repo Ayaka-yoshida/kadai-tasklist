b05acc598b37343260f029c555c08ac0790caac6
ayaka.yoshida <eveaeve524@gmail.com>
2019-05-23 01:46:40 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      # current_user ：サインインしているユーザを取得
+      @task = current_user.tasks.build #form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 end
